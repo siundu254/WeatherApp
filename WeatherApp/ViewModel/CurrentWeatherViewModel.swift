ffcfd9c584758152a5899320078dc5ea7ff9d18d
Kevin Siundu <kevin.siundu@equitybank.co.ke>
2021-06-07 23:00:01 +0300

    beginning work on view controller

--- WeatherApp/ViewModel/CurrentWeatherViewModel.swift
+++ WeatherApp/ViewModel/CurrentWeatherViewModel.swift
@@ -8,46 +8,11 @@
 import Foundation
 import Combine
 
-class CurrentWeatherViewModel: ObservableObject {
+class CurrentWeatherViewModel {
+    private var state = State()
     
-    let didChange = PassthroughSubject<CurrentWeatherResponse, Never>()
-    var subscriptions = Set<AnyCancellable>()
-    let networkClient = NetworkClient()
-    
-    @Published var currentWeather: CurrentWeatherResponse?
-    @Published var forecastWeather: ForecastResponse?
-    
-    init() {
-        // get Current Weather and Forecast data
-        getCurrentWeather()
-        getForecastFive()
-    }
-    
-    func getCurrentWeather() {
-        let weatherRepo = WeatherRepository(networkClient: networkClient)
-        weatherRepo.getCurrentWeather().sink(receiveCompletion: { (completion) in
-            switch completion {
-            case let .failure(error):
-                // error decoding data
-                print("Error = \(error.localizedDescription)")
-            case .finished: break
-            }
-        }) { current in
-            self.currentWeather = current
-        }.store(in: &subscriptions)
-    }
-    
-    func getForecastFive() {
-        let weatherRepo = WeatherRepository(networkClient: networkClient)
-        weatherRepo.getForecastFive().sink(receiveCompletion: { (completion) in
-            switch completion {
-            case let .failure(error):
-                print("Error = \(error.localizedDescription)")
-            case .finished: break
-            }
-        }) { forecast in
-            self.forecastWeather = forecast
-        }.store(in: &subscriptions)
+    init(model: CurrentWeatherResponse?) {
+        self.state.currentWeather = model
     }
 
     private lazy var dateFormatter: DateFormatter = {
@@ -63,8 +28,8 @@ class CurrentWeatherViewModel: ObservableObject {
     }()
     
     var weatherIcon: String {
-        if self.currentWeather?.weather.count ?? 0 > 0 {
-            return self.currentWeather?.weather[0].icon ?? "sun.max.fill"
+        if self.state.currentWeather?.weather.count ?? 0 > 0 {
+            return self.state.currentWeather?.weather[0].icon ?? "sun.max.fill"
         }
         return "sun.max.fill"
     }
@@ -74,27 +39,35 @@ class CurrentWeatherViewModel: ObservableObject {
     }
     
     var temperature: String {
-        return getTemperatureWithFormat(temp: self.currentWeather?.main.temp ?? 0.0)
+        if self.state.currentWeather?.main.temp ?? 0.0 > 0 {
+            return getTemperatureWithFormat(temp: self.state.currentWeather?.main.temp ?? 0.0)
+        }
+        return "0.0"
     }
     
     var conditions: String {
-        if self.currentWeather?.weather.count ?? 0 > 0 {
-            return self.currentWeather?.weather[0].main ?? "sunny"
+        if self.state.currentWeather?.weather.count ?? 0 > 0 {
+            return self.state.currentWeather?.weather[0].main ?? "sunny"
         }
         
         return "sunny"
     }
     
     var minTemperature: String {
-        return getTemperatureWithFormat(temp: self.currentWeather?.main.temp_min ?? 0.0)
+        return getTemperatureWithFormat(temp: self.state.currentWeather?.main.temp_min ?? 0.0)
     }
     
     var maxTemperature: String {
-        return getTemperatureWithFormat(temp: self.currentWeather?.main.temp_max ?? 0.0)
+        return getTemperatureWithFormat(temp: self.state.currentWeather?.main.temp_max ?? 0.0)
     }
     
     func getDayFor(timestamp: Int) -> String {
         return dayFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(timestamp)))
+    }
+    
+    struct State {
+        var currentWeather: CurrentWeatherResponse?
+        var forecastWeather: ForecastResponse?
     }
     
 }
--- WeatherApp/views/ViewController.swift
+++ WeatherApp/views/ViewController.swift
@@ -9,22 +9,47 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-
-    lazy var viewModel: CurrentWeatherViewModel = {
-        let viewModel = CurrentWeatherViewModel()
-        return viewModel
-    }()
+    
+    private var state = State()
+    var subscriptions = Set<AnyCancellable>()
+    let networkClient = NetworkClient()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        loadModel()
     }
     
-    func loadModel() {
-        viewModel.$currentWeather.sink{[weak self] current in
-        }.store(in: &cancellables)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getCurrentWeather()
+    }
+    
+    func getCurrentWeather() {
+        let weatherRepo = WeatherRepository(networkClient: networkClient)
+        weatherRepo.getCurrentWeather()
+            .sink(
+                receiveCompletion: {(completion) in
+                    switch completion {
+                    case let .failure(error):
+                        print("Error = \(error.localizedDescription)")
+                    case .finished: break
+                    }
+                },
+                receiveValue: {(current) in
+                    self.state.currentWeather = current
+                    self.updateUI(model: current)
+                })
+            .store(in: &subscriptions)
+    }
+    
+    func updateUI(model: CurrentWeatherResponse) {
+        let model = CurrentWeatherViewModel(model: self.state.currentWeather)
+        print("conditions = \(model.conditions)")
+    }
+    
+    struct State {
+        var currentWeather: CurrentWeatherResponse?
+        var forecastWeather: ForecastResponse?
     }
 }
 
